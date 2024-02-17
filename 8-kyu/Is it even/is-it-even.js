f695f7695a53d5de27d3fb841db40f8e75efe3c4
Adrian Gaile Singh <adriangailee@gmail.com>
2024-02-17 12:00:24 +0800

    Add new solution

---
+++ 8-kyu/Is it even/is-it-even.js
@@ -0,0 +1,6 @@
+function testEven(n) {
+    if (n % 2 == 0) {
+        return true;
+    }
+    return false;
+}
---
+++ 8-kyu/simple-multiplication.js
@@ -0,0 +1,8 @@
+function simpleMultiplication(number) {
+    if (number % 2 == 0 ) {
+      return number * 8;
+    }
+    else if (number % 2 == 1) {
+      return number * 9;
+    }
+}
