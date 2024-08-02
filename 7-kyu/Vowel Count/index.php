3173a82b195103f69705a1ff277000a563a5e059
Adrian Gaile Singh <adriangaile.singh@gmail.com>
2024-08-02 22:33:55 +0800

    Chore: Solve a 7 kyu problem

---
+++ 7-kyu/Vowel Count/index.php
@@ -0,0 +1,16 @@
+<?php
+/*TODO: Return the number (count) of vowels in the given string. The input string will only consist of lower case letters */
+/*
+1. Count the vowel occurences using substr_count
+*/
+function getCount($str)
+{
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $vowelsCount = 0;
+
+    foreach ($vowels as $vowel) {
+        $vowelsCount += substr_count($str, $vowel);
+    }
+    echo $vowelsCount;
+}
+getCount('abracadabra');
